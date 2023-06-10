d94d9f89f3c5887daca36df0344ed9e4f2f0b350
Mohamed <mohamedbenjdira5@gmail.com>
2023-06-10 11:22:02 +0100

    implementing the location adder function

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,14 @@ const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 dotenv.config();
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,119 @@
-import { useState } from "react";
-import Map, { Marker } from "react-map-gl";
+import { useEffect, useState } from "react";
+import Map, { Marker, Popup } from "react-map-gl";
 import RoomIcon from "@mui/icons-material/Room";
+import Star from "@mui/icons-material/Star";
+import "./app.css";
+import axios from "axios";
+import { format } from "timeago.js";
 
 function App() {
+  const [pins, setPins] = useState([]);
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
+  const [newPlace, setNewPlace] = useState(null);
   const [viewState, setViewState] = useState({
-    longitude: 10,
-    latitude: 36,
+    longitude: 2.333333,
+    latitude: 48.866667,
     zoom: 4,
   });
+
+  const handleMarkerClick = (id) => {
+    setCurrentPlaceId(id);
+  };
+
+  const handleAddClick = (e) => {
+    const { lng, lat } = e.lngLat;
+    setNewPlace({
+      lat: lat,
+      long: lng,
+    });
+  };
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("http://localhost:8000/api/pins");
+        setPins(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, []);
   return (
-    <div>
+    <div className="app">
       <Map
         {...viewState}
         onMove={(evt) => setViewState(evt.viewState)}
-        style={{ width: "100vw", height: "100vh" }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
-        mapboxAccessToken="pk.eyJ1IjoiaGFtYWFhYSIsImEiOiJjbGluZHhwbmIwNGhsM3BxY3Z1aHc4a2JwIn0.xInFxM3fTCGMgd90O1Qvmg"
+        style={{ width: "100vw", height: "100vh" }}
+        onDblClick={handleAddClick}
+        mapboxAccessToken="pk.eyJ1IjoiaGFtYWFhYSIsImEiOiJjbGlwNHphaGIwbTh0M2RwY3Zub3Zkc2E1In0.ZkrArC3cptvPSmfXzeeR7Q"
       >
-        <Marker latitude={36} longitude={10} offsetLeft={-20} offsetTop={-10}>
-          <RoomIcon />
-        </Marker>
+        {pins.map((pin) => (
+          <>
+            <Marker
+              latitude={pin.lat}
+              longitude={pin.long}
+              onClick={() => setShowPopup(true)}
+            >
+              <RoomIcon
+                style={{
+                  fontSize: viewState.zoom * 7,
+                  color: "slateblue",
+                  cursor: "pointer",
+                }}
+                onClick={() => handleMarkerClick(pin._id)}
+              />
+            </Marker>
+            ;
+            {currentPlaceId === pin._id && showPopup && (
+              <Popup
+                longitude={pin.long}
+                latitude={pin.lat}
+                anchor="left"
+                closeButton={true}
+                closeOnClick={false}
+                onClose={() => setShowPopup(false)}
+              >
+                <div className="card">
+                  <label>Place</label>
+                  <h4 className="place">{pin.title}</h4>
+                  <label>Review</label>
+                  <p className="desc">{pin.desc}</p>
+                  <label>Rating</label>
+                  <div className="stars">
+                    <Star className="star" />
+                    <Star className="star" />
+                    <Star className="star" />
+                    <Star className="star" />
+                    <Star className="star" />
+                  </div>
+                  <label>Information</label>
+                  <span className="username">
+                    Created by <b>{pin.username}</b>
+                  </span>
+                  <span className="date">{format(pin.createdAt)}</span>
+                </div>
+              </Popup>
+            )}
+          </>
+        ))}
+        {newPlace && (
+          <Marker
+            latitude={newPlace?.lat}
+            longitude={newPlace?.long}
+            onClick={() => setShowPopup(true)}
+          >
+            <RoomIcon
+              style={{
+                fontSize: viewState.zoom * 7,
+                color: "slateblue",
+                cursor: "pointer",
+              }}
+            />
+          </Marker>
+        )}
       </Map>
     </div>
   );
